4082047beb4877e580e338a1bdf46bab2180ecc4
Edgar Ortiz <ed.ortizm@gmail.com>
2020-04-08 15:30:14 -0400

    adding filters. Combiantion of parameters working

--- main_parallel.py
+++ main_parallel.py
@@ -1,5 +1,6 @@
 from models_dust import *
-
+from multiprocessing import Pool
+from itertools import product
 # Function to obtain umin, umax lists
 def umm(files_array):
     umin, umax = [], []
@@ -12,4 +13,12 @@ def umm(files_array):
     umin.remove('U1.00')
     umax = set(umax)
     return umin, umax
+
+#Computing the grid of models
 # https://stackoverflow.com/questions/5442910/python-multiprocessing-pool-map-for-multiple-arguments
+# if __name__ == '__main__':
+#     names = ['Brown', 'Wilson', 'Bartlett', 'Rivera', 'Molloy', 'Opie']
+#     with multiprocessing.Pool(processes=3) as pool:
+#         results = pool.starmap(merge_names, product(names, repeat=2))
+#     print(results)
+# model = Model(umin= '0.20', umax='1e3', model='MW3.1_60',gamma = 0.3,data = d, filter=filter)
--- parallel.py
+++ parallel.py
@@ -6,9 +6,22 @@ from main_parallel import *
 # Lists of physical parameters for a model
 data = Data()
 dirpath, dirnames, files_array = data.txt_files()
-umin, umax = umm(files_array)
+umins, umaxs = umm(files_array)
 # models --> it is already on moddels_dust
-gamma = np.logspace(-3.,-0.3,10)
+gammas = np.logspace(-3.,-0.3,10)
 
-# Using Pool.starmap to manage xxxxxx xxxxxx iterators
+# Permutations of the physical parameters to generate the tuples for starmap
+# https://stackoverflow.com/questions/798854/all-combinations-of-a-list-of-lists
+params      = [umins,umaxs,models,gammas]
+params_comb = list(product(*params))
+# 102300 combinations: mamma mia
+# Using Pool.starmap to manage the sub-processes
+
 # https://stackoverflow.com/questions/5442910/python-multiprocessing-pool-map-for-multiple-arguments
+#
+# def f(x,y):
+#     return x*y
+# if __name__ == '__main__':
+#     with Pool() as pool:
+#         results = pool.starmap(f, [(1,2),(3,4),(5,6)])
+#     print(results)
